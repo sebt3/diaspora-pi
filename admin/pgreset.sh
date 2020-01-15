b9d5cde2a6bf1c42508eef4c45a779d0dd077ff4
Sébastien HUSS <sebastien.huss@gmail.com>
2020-01-15 23:55:41 +0100

    added pgreset script

---
+++ admin/pgreset.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+echo "Warning this script is dangerous !!!!"
+
+
+hoSt=${1:-"pi"}
+
+fail() {
+	r=$?
+	echo $@
+	exit $r
+}
+
+Stack=diaspora
+Svc=postgres
+getCont() {
+	ssh "$hoSt" sudo docker stack ps ${Stack} -f name=${Stack}_${Svc}.1 --no-trunc|awk -v N=${Stack}_${Svc}.1 '$2==N{print $1}'
+}
+getVolumePath() {
+	ssh "$hoSt" sudo docker volume inspect diaspora_database --format '{{.Mountpoint}}'
+}
+sql() {
+	ssh "$hoSt" sudo docker exec -i ${Stack}_${Svc}.1.$Cont su - postgres -c "'psql -U diaspora -d diaspora_production -t'"|sed '/^$/d'
+}
+
+echo "Stop the containers"
+ssh "$hoSt" sudo docker service scale diaspora_postgres=0 diaspora_diaspora=0 diaspora_nginx=0
+
+echo "Get the postgres volume path"
+voL=$(getVolumePath)
+
+echo "Running pg_resetwal"
+ssh "$hoSt" sudo docker run -it --rm -v "$voL:/var/lib/postgresql/data" postgres:12-alpine su - postgres -c "'pg_resetwal /var/lib/postgresql/data'"
+
+echo "Start the database"
+ssh "$hoSt" sudo docker service scale diaspora_postgres=1
+echo "Start Diaspora*"
+ssh "$hoSt" sudo docker service scale diaspora_diaspora=1 diaspora_nginx=1
+
--- analysis/step1_data_load/README.md
+++ analysis/step1_data_load/README.md
@@ -2,14 +2,14 @@
 
 ## First step
 
-The goal is to load as much post as possible as fast as possible.
+The goal is to load as much posts as possible as fast as possible.
 One option would have been to insert lines manually, but then, it wouldnt be real-world data. Same goes with automatic posting. 
 
 So the solution was to collect post from real users.
 To do so, the idea is to "follow" as much users as possible.
 Once a D* user interact with a collected post, it become known in the database.
-The script import.sh look for users known but not yet followed and set a friend request.
-The grow_report.sh generate a post with some statistics about the current status of the process.
+The script `import.sh` look for users known but not yet followed and set a friend request.
+The `grow_report.sh` generate a post with some statistics about the current status of the process.
 
 Both scripts depend on having [chromedriver](https://chromedriver.chromium.org/) installed and started on localhost.
 arguments are :
